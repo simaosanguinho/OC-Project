8b9b10c6948515adb9e0fd8a1a5326de621744fb
simaosanguinho <simao.sanguinho@gmail.com>
2023-10-02 14:42:57 +0100

    lol

--- Lab02/src/Cache.h
+++ Lab02/src/Cache.h
@@ -1,8 +1,10 @@
+#define L1_N_LINES 16
+
 #define WORD_SIZE 4                 // in bytes, i.e 32 bit words
-#define BLOCK_SIZE 2 * WORD_SIZE    // in bytes
-#define DRAM_SIZE 1024 * BLOCK_SIZE // in bytes
-#define L1_SIZE 1 * BLOCK_SIZE      // in bytes
-#define L2_SIZE 512 * BLOCK_SIZE    // in bytes
+#define BLOCK_SIZE (2 * WORD_SIZE)    // in bytes
+#define DRAM_SIZE (1024 * BLOCK_SIZE) // in bytes
+#define L1_SIZE (L1_N_LINES * BLOCK_SIZE)      // in bytes - 16 Lines
+#define L2_SIZE (512 * BLOCK_SIZE)    // in bytes
 
 #define MODE_READ 1
 #define MODE_WRITE 0
