f0a2e2a7de4ce8765884dad265515b37dd56b4b7
pearsetting44 <jose.a.pereira03@gmail.com>
2023-10-11 21:49:33 +0100

    Clean L2W2Cache.c

--- Lab02/task03/L2W2Cache.c
+++ Lab02/task03/L2W2Cache.c
@@ -27,74 +27,73 @@ void accessDRAM(uint32_t address, unsigned char *data, uint32_t mode) {
 }
 
 /*********************** L1 & L2 cache *************************/
-void initCache() { 
+void initCache() {
   cache.L1cache.init = 0;
-  cache.L2cache.init = 0; 
+  cache.L2cache.init = 0;
 }
 
 int log_base2(int x) {
-    if (x == 0) {
-        return 0;
-    }
+  if (x == 0) {
+    return 0;
+  }
 
-    int result = 1.0;
-    int increment = 1.0;
-    while (x > 2.0) {
-        x /= 2.0;
-        result += increment;
-    }
-    return result;
+  int result = 1;
+  int increment = 1;
+  while (x > 2) {
+    x /= 2;
+    result += increment;
+  }
+  return result;
 }
 
 void accessL1(uint32_t address, unsigned char *data, uint32_t mode) {
-
-  uint32_t Tag, Index, MemAddress, Offset;
-  // uint32_t Offset;
+  uint32_t Tag, Index, MemAddress, Offset, indexBits, offsetBits;
   unsigned char TempBlock[BLOCK_SIZE];
-  memset(TempBlock, 0, BLOCK_SIZE);
-  int indexBits, offsetBits;
+  memset(TempBlock, 0, BLOCK_SIZE); // set all values to 0
 
   /* init cache */
   if (cache.L1cache.init == 0) {
     for (int i = 0; i < L1_N_LINES; i++) {
-      cache.L1cache.lines[i].Valid = 0;
+      cache.L1cache.lines[i].Valid = 0; // set all valid bits to 0
     }
-    cache.L1cache.init = 1;
+    cache.L1cache.init = 1; // set init to 1
   }
 
-  CacheLine *Lines = cache.L1cache.lines;
-  indexBits = log_base2(L1_N_LINES); // 8 bits
-  offsetBits = 6;
+  CacheLine *Lines = cache.L1cache.lines; // get lines from L1
 
-  // save offset for later
-  Offset = address << (32 - offsetBits);
-  Offset = Offset >> (32 - offsetBits);
+  offsetBits = log_base2(BLOCK_SIZE);    // how many bits for offset
+  Offset = address << (32 - offsetBits); // shift address to the left
+  Offset =
+      Offset >>
+      (32 - offsetBits); // shift address to the right, so we get the offset
 
-  Index = address << (32 - indexBits - offsetBits);
-  Index = Index >> (32 - indexBits);
+  indexBits = log_base2(L1_N_LINES);                // how many bits for index
+  Index = address << (32 - indexBits - offsetBits); // shift address to the left
+  Index = Index >>
+          (32 - indexBits); // shift address to the right, so we get the index
 
-  Tag = address >> (indexBits + offsetBits);
+  Tag = address >>
+        (indexBits + offsetBits); // get tag, by shifting address to the right
 
-
-  MemAddress = address >> offsetBits; // again this....!
+  MemAddress = address >> offsetBits;    // again this....!
   MemAddress = MemAddress << offsetBits; // address of the block in memory
 
   /* access Cache*/
   CacheLine *Line = &(Lines[Index]);
 
-  if (!Line->Valid || Line->Tag != Tag) {         // if block not present - miss
-    accessL2(address, TempBlock, MODE_READ); // get new block from DRAM
-    if ((Line->Valid) && (Line->Dirty)) { // line has dirty block
+  if (!Line->Valid || Line->Tag != Tag) {        // if block not present - MISS
+    accessL2(address, TempBlock, MODE_READ);     // get new block from DRAM
+    if ((Line->Valid) && (Line->Dirty)) {        // if line has dirty block
       accessL2(address, Line->Data, MODE_WRITE); // then write back old block
     }
     memcpy(&(Line->Data), TempBlock,
            BLOCK_SIZE); // copy new block to cache line
-    Line->Valid = 1;
-    Line->Tag = Tag;
-    Line->Dirty = 0;
-  } // if miss, then replaced with the correct block
+    Line->Valid = 1;    // set valid bit to 1
+    Line->Tag = Tag;    // set tag to the new tag
+    Line->Dirty = 0;    // set dirty bit to 0, because we just read the block
+  }                     // if miss, then replaced with the correct block
 
-  if (mode == MODE_READ) {    // read data from cache line
+  if (mode == MODE_READ) { // read data from cache line
     memcpy(data, &(Line->Data[Offset]), WORD_SIZE);
     time += L1_READ_TIME;
   }
@@ -102,92 +101,96 @@ void accessL1(uint32_t address, unsigned char *data, uint32_t mode) {
   if (mode == MODE_WRITE) { // write data from cache line
     memcpy(&(Line->Data[Offset]), data, WORD_SIZE);
     time += L1_WRITE_TIME;
-    Line->Dirty = 1;
+    Line->Dirty = 1; // set dirty bit to 1, because we wrote to the block
   }
 }
 
-
 void accessL2(uint32_t address, unsigned char *data, uint32_t mode) {
 
-  uint32_t Tag, Index, MemAddress, Offset;
-  // uint32_t Offset;
+  uint32_t Tag, Index, MemAddress, Offset, indexBits, offsetBits;
   unsigned char TempBlock[BLOCK_SIZE];
-  int indexBits, offsetBits;
+  memset(TempBlock, 0, BLOCK_SIZE); // set all values to 0
 
   /* init cache */
   if (cache.L2cache.init == 0) {
-    for (int i = 0; i < L2_N_LINES; i++) {
+    for (int i = 0; i < L2_N_LINES; i++) { // since we have 2 blocks per line
       cache.L2cache.lines[i].Valid[0] = 0;
       cache.L2cache.lines[i].Valid[1] = 0;
+      /* The line that will be replace is the one that has the oldest time (= 1)
+       */
       cache.L2cache.lines[i].Time[0] = 1;
       cache.L2cache.lines[i].Time[1] = 0;
-      // change the line that has the oldest time = 1
     }
     cache.L2cache.init = 1;
   }
 
-  CacheL2Line *Lines = cache.L2cache.lines;
-  indexBits = log_base2(L2_N_LINES); // 8 bits
-  offsetBits = 6;
+  CacheL2Line *Lines = cache.L2cache.lines; // get lines from L2
+  offsetBits = log_base2(BLOCK_SIZE);       // how many bits for offset
+  Offset = address << (32 - offsetBits);    // shift address to the left
+  Offset =
+      Offset >>
+      (32 - offsetBits); // shift address to the right, so we get the offset
 
-  // save offset for later
-  Offset = address << (32 - offsetBits);
-  Offset = Offset >> (32 - offsetBits);
+  indexBits = log_base2(L2_N_LINES);                // how many bits for index
+  Index = address << (32 - indexBits - offsetBits); // shift address to the left
+  Index = Index >>
+          (32 - indexBits); // shift address to the right, so we get the index
 
-  //Offset = address & 0x7; // 6 LSBs
-  Index = address << (32 - indexBits - offsetBits);
-  Index = Index >> (32 - indexBits);
+  Tag = address >>
+        (indexBits + offsetBits); // get tag, by shifting address to the right
 
-  Tag = address >> (indexBits + offsetBits);
-
-
-  // TODO: memadress == tag or tag + index
-  MemAddress = address >> offsetBits; // again this....!
+  MemAddress =
+      address >>
+      offsetBits; // shift address to the right, so we can remove the offset
   MemAddress = MemAddress << offsetBits; // address of the block in memory
 
   /* access Cache*/
-  CacheL2Line *Line = &(Lines[Index]);
-  int found = 0;
-  int l2line_block = 0;
-  for (int i = 0; i < L2_ASSOC; i++) {
-    if (Line->Valid[i] && Line->Tag[i] == Tag) {
-      found = 1;
-      l2line_block = i;
+  CacheL2Line *Line = &(Lines[Index]); // get the line from L2
+
+  int found = 0;         // flag to check if we found the block
+  int L2_line_block = 0; // which block in the line we are looking at
+  for (int i = 0; i < L2_ASSOC;
+       i++) { // iterate over the blocks in the line (associativity)
+    if (Line->Valid[i] && Line->Tag[i] == Tag) { // if we found the block
+      found = 1;                                 // set flag to 1
+      L2_line_block = i;                         // save the block we found
       break;
     }
   }
 
-  if (!found) {         // if block not present - miss
+  if (!found) {                                   // if block not present - Miss
     accessDRAM(MemAddress, TempBlock, MODE_READ); // get new block from DRAM
 
-    for (int i = 0; i < L2_ASSOC; i++) {
-      if (Line->Time[i] == 1) {
-        l2line_block = i;
+    for (int i = 0; i < L2_ASSOC; i++) { // iterate over the blocks in the line
+      if (Line->Time[i] == 1) { // if we found the block to replace, aka
+                                // the oldest one
+        L2_line_block = i;      // save the block we found
         break;
       }
     }
 
-    if ((Line->Valid[l2line_block]) && (Line->Dirty[l2line_block])) { // line has dirty block
-      accessDRAM(MemAddress, &(Line->Data[l2line_block * BLOCK_SIZE]), MODE_WRITE); // then write back old block
+    if ((Line->Valid[L2_line_block]) &&
+        (Line->Dirty[L2_line_block])) { // line has dirty block
+      accessDRAM(MemAddress, &(Line->Data[L2_line_block * BLOCK_SIZE]),
+                 MODE_WRITE); // then write back old block to DRAM
     }
 
-    memcpy(&(Line->Data[l2line_block * BLOCK_SIZE]), TempBlock,
-           BLOCK_SIZE); // copy new block to cache line
-    Line->Valid[l2line_block] = 1;
-    Line->Tag[l2line_block] = Tag;
-    Line->Dirty[l2line_block] = 0; 
+    memcpy(&(Line->Data[L2_line_block * BLOCK_SIZE]), TempBlock,
+           BLOCK_SIZE);             // copy new block to cache line
+    Line->Valid[L2_line_block] = 1; // set valid bit to 1
+    Line->Tag[L2_line_block] = Tag; // set tag to the new tag
+    Line->Dirty[L2_line_block] = 0; // set dirty bit to 0, because we just read
+                                    // the block
 
-    
-    
   } // if miss, then replaced with the correct block
 
-  if (mode == MODE_READ) {    // read data from cache line
+  if (mode == MODE_READ) { // read data from cache line
     memcpy(data, &(Line->Data[Offset]), WORD_SIZE);
-    // alter last recently used  
-    if (l2line_block == 0) {
-      Line->Time[0] = 0;
-      Line->Time[1] = 1;
-    } else {
+    // alter last recently used
+    if (L2_line_block == 0) { // if we read from the first block
+      Line->Time[0] = 0;      // set that first block to the most recently used
+      Line->Time[1] = 1;      // and the second block to the least recently used
+    } else {                  // else, do the opposite
       Line->Time[0] = 1;
       Line->Time[1] = 0;
     }
@@ -196,19 +199,19 @@ void accessL2(uint32_t address, unsigned char *data, uint32_t mode) {
 
   if (mode == MODE_WRITE) { // write data from cache line
     memcpy(&(Line->Data[Offset]), data, WORD_SIZE);
-    // alter last recently used  
-    if (l2line_block == 0) {
-      Line->Time[0] = 0;
-      Line->Time[1] = 1;
-    } else {
+    // alter last recently used
+    if (L2_line_block == 0) { // if we wrote to the first block
+      Line->Time[0] = 0;      // set that first block to the most recently used
+      Line->Time[1] = 1;      // and the second block to the least recently used
+    } else {                  // else, do the opposite
       Line->Time[0] = 1;
       Line->Time[1] = 0;
     }
     time += L2_WRITE_TIME;
-    Line->Dirty[l2line_block] = 1;
+    Line->Dirty[L2_line_block] = 1; // set dirty bit to 1, because we wrote to
+                                    // the block
   }
 }
-
 
 /*********************** Read and Write *************************/
 
