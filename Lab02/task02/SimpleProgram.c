0863577a278f0bbc98259af233db07b595f75836
Pedro Ribeiro <p.m.ribeiro16@gmail.com>
2023-10-13 15:29:23 +0100

    testa la

--- Lab02/task02/SimpleProgram.c
+++ Lab02/task02/SimpleProgram.c
@@ -1,51 +1,66 @@
-#include "L2Cache.h"
+#include "../task02/Cache.h"
+#include "../task02/L2Cache.h"
+#include <assert.h>
 
 int main() {
+  resetTime();
+  initCache();
 
-  // set seed for random number generator
-  srand(0);
 
-  int clock1, value;
+  /** 
+  This is our big stress test, touch every memory word!! :nerdemoji:
 
-  for(int n = 1; n <= DRAM_SIZE/4; n*=WORD_SIZE) {
+  We fill the cache with dirty blocks and then replace them all with more
+  dirty blocks untill all memory is touched
+  */
 
-    resetTime();
-    initCache();
+  uint32_t value = 1;
+  write(0, (uint8_t *)(&value));
 
-    printf("\nNumber of words: %d\n", (n-1)/WORD_SIZE + 1);
-    
-    for(int i = 0; i < n; i+=WORD_SIZE) {
-      write(i, (unsigned char *)(&i));
-      clock1 = getTime();
-      printf("Write; Address %d; Value %d; Time %d\n", i, i, clock1);
-    }
+  value = 1;
+  write(800, (uint8_t *)(&value));
 
-    for(int i = 0; i < n; i+=WORD_SIZE) {
-      read(i, (unsigned char *)(&value));
-      clock1 = getTime();
-      printf("Read; Address %d; Value %d; Time %d\n", i, value, clock1);
-      
-    }  
-  }
+  value = 1;
+  write(123, (uint8_t *)(&value));
 
-  printf("\nRandom accesses\n");
+  value = 1;
+  write(4928, (uint8_t *)(&value));
 
-  // Do random accesses to the cache
-  for(int i = 0; i < 100; i++) {
-    int address = rand() % (DRAM_SIZE/4);
-    address = address - address % WORD_SIZE;
-    int mode = rand() % 2;
-    if (mode == MODE_READ) {
-      read(address, (unsigned char *)(&value));
-      clock1 = getTime();
-      printf("Read; Address %d; Value %d; Time %d\n", address, value, clock1);
-    }
-    else {
-      write(address, (unsigned char *)(&address));
-      clock1 = getTime();
-      printf("Write; Address %d; Value %d; Time %d\n", address, address, clock1);
+  value = 1;
+  write(12093, (uint8_t *)(&value));
+
+  // fill cache with fully written blocks and force write back on every new fill
+  for (int x = 0; x < 4; ++x) {
+    for (int i = 0; i < 256; ++i) {
+      for (int j = 0; j < 16; ++j) {
+        read(16384*x + 64*i + 4*j, (uint8_t *)(&value));
+      }
     }
   }
-  
+
+ for (int i = 0; i < 256; ++i) {
+    read(64*i, (uint8_t *)(&value));
+  }
+
+  read(0, (uint8_t *)(&value));
+  printf("Result: %d\n", value);
+
+  read(800, (uint8_t *)(&value));
+  printf("Result: %d\n", value);
+
+  read(123, (uint8_t *)(&value));
+  printf("Result: %d\n", value);
+
+  read(4928, (uint8_t *)(&value));
+  printf("Result: %d\n", value);
+
+  read(12093, (uint8_t *)(&value));
+  printf("Result: %d\n", value);
+
+  // last but not least, force write back on the last blocks loaded
+ 
+//   assert(total_time == expected_time && "Expects a memory access for every block in cache, twice!");
+  printf("Successful test!\n");
+
   return 0;
 }
